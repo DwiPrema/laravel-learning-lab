a7478517a79492b95c3250a83f2b7c6bad03c36a
DwiPrema <madedwiprema08@gmail.com>
2026-06-06 13:29:31 +0800

    feat(blade template): learn about html encoding and escaping output

--- routes/blade-template.php
+++ routes/blade-template.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function (){
+Route::get('/hello', function () {
     return view('blade-template.hello', ["name" => "Dwi Premayasa"]);
+});
+
+Route::get('/html-encoding', function (Request $request) {
+    return view('blade-template.html-encoding', ["name" => $request->input("name")]);
 });
