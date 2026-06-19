5d57dd601e5bae360fbfe006a417508727cf3819
DwiPrema <madedwiprema08@gmail.com>
2026-06-19 10:02:33 +0800

    feat(blade-template): add while loop directives usage example

--- tests/Feature/BladeTemplateTest.php
+++ tests/Feature/BladeTemplateTest.php
@@ -94,4 +94,19 @@ class BladeTemplateTest extends TestCase
         ->assertSeeText("Dwi")
         ->assertSeeText("Texas");
     }
+
+    public function testWhile()
+    {
+        $this->view("blade-template.while", ["i" => 0])
+        ->assertSeeText("The current value is 0")
+        ->assertSeeText("The current value is 1")
+        ->assertSeeText("The current value is 2")
+        ->assertSeeText("The current value is 3")
+        ->assertSeeText("The current value is 4")
+        ->assertSeeText("The current value is 5")
+        ->assertSeeText("The current value is 6")
+        ->assertSeeText("The current value is 7")
+        ->assertSeeText("The current value is 8")
+        ->assertSeeText("The current value is 9");
+    }
 }
