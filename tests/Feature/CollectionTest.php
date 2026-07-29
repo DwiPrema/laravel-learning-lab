19220cea6c8f7210d67b71d4f3ef2d4a526a6325
DwiPrema <madedwiprema08@gmail.com>
2026-07-29 10:10:34 +0800

    (collection): learn slicing concepts

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -282,4 +282,13 @@ class CollectionTest extends TestCase
             ])
         ], $result->all());
     }
+
+    public function testSlice(){
+        $collection = collect([1,2,3,4,5,6,7,8,9]);
+        $result = $collection->slice(3);
+        $this->assertEqualsCanonicalizing([4,5,6,7,8,9], $result->values()->all());
+
+        $result = $collection->slice(3,2);
+        $this->assertEqualsCanonicalizing([4,5], $result->values()->all());
+    }
 }
