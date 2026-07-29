68235910a8581669b9223c72df0d6b39014f07ff
DwiPrema <madedwiprema08@gmail.com>
2026-07-29 08:44:54 +0800

    (collection): learn zipping concepts

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -10,14 +10,14 @@ class CollectionTest extends TestCase
     public function testCreateCollection()
     {
         $collection = collect([1, 2, 3]);
-        $this->assertEqualsCanonicalizing([1, 2,3], $collection->all());
+        $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
     }
 
     public function testForEach()
     {
-        $collection = collect([1,2,3,4,5,6,7,8,9]);
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
-        foreach($collection as $key => $value){
+        foreach ($collection as $key => $value) {
             $this->assertEquals($key + 1, $value);
         }
     }
@@ -25,22 +25,22 @@ class CollectionTest extends TestCase
     public function testCrud()
     {
         $collection = collect([]);
-        $collection->push(1,2,3);
-        $this->assertEqualsCanonicalizing([1,2,3], $collection->all());
+        $collection->push(1, 2, 3);
+        $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
 
         $result = $collection->pop();
         $this->assertEquals(3, $result);
-        $this->assertEqualsCanonicalizing([1,2], $collection->all());
+        $this->assertEqualsCanonicalizing([1, 2], $collection->all());
     }
 
     public function testMap()
     {
-        $collection = collect([1,2,3]);
+        $collection = collect([1, 2, 3]);
         $result = $collection->map(function ($value) {
             return $value * 2;
         });
 
-        $this->assertEqualsCanonicalizing([2,4,6], $result->all());
+        $this->assertEqualsCanonicalizing([2, 4, 6], $result->all());
     }
 
     public function testMapInto()
@@ -91,5 +91,51 @@ class CollectionTest extends TestCase
             "IT" => collect(["Dwi", "Prema"]),
             "HR" => collect(["Yasa"]),
         ], $result->all());
+    }
+
+    public function testZip()
+    {
+        $collection1 = collect([1, 2, 3]);
+        $collection2 = collect([4, 5, 6]);
+        $collection3 = $collection1->zip($collection2);
+
+        $this->assertEquals([
+            collect([1, 4]),
+            collect([2, 5]),
+            collect([3, 6]),
+        ], $collection3->all());
+    }
+
+    public function testConcat()
+    {
+        $collection1 = collect([1, 2, 3]);
+        $collection2 = collect([4, 5, 6]);
+        $collection3 = $collection1->concat($collection2);
+
+        $this->assertEquals([1, 2, 3, 4, 5, 6], $collection3->all());
+    }
+
+    public function testCombine()
+    {
+        $collection1 = ["name", "country", "age"];
+        $collection2 = ["Dwi", "Texas", "16"];
+        $collection3 = ["Prema", "United States", "17"];
+        $collection4 = collect([
+            collect($collection1)->combine($collection2),
+            collect($collection1)->combine($collection3),
+        ]);
+
+        $this->assertEqualsCanonicalizing([
+            [
+                "name" => "Dwi",
+                "country" => "Texas",
+                "age" => "16",
+            ],
+            [
+                "name" => "Prema",
+                "country" => "United States",
+                "age" => "17",
+            ],
+        ], $collection4->toArray());
     }
 }
