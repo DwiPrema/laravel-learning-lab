f6c4263a5a6d46acd183a38862106127db349531
DwiPrema <madedwiprema08@gmail.com>
2026-07-30 10:43:17 +0800

    (laravel-database): Database Transaction

--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -4,19 +4,23 @@ namespace Tests\Feature;
 
 use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
+use \Illuminate\Database\QueryException;
 
-class DatabaseTest extends TestCase 
+class DatabaseTest extends TestCase
 {
-    protected function setUp(): void 
+    protected function setUp(): void
     {
         parent::setUp();
         DB::delete('delete from categories');
     }
 
-    public function testCrud() 
+    public function testCrud()
     {
         DB::insert("insert into categories(id_categories, name, description, created_at) values(?, ?, ?, ?)", [
-            1, "Gadget", "Gadget Categories", "2020-10-10 10:10:10"
+            1,
+            "Gadget",
+            "Gadget Categories",
+            "2020-10-10 10:10:10"
         ]);
 
         $result = DB::select('select * from categories where id_categories = ?', [1]);
@@ -28,7 +32,7 @@ class DatabaseTest extends TestCase
         self::assertEquals("2020-10-10 10:10:10", $result[0]->created_at);
     }
 
-    public function testCrudNamedParameter() 
+    public function testCrudNamedParameter()
     {
         DB::insert("insert into categories(id_categories, name, description, created_at) values(:id_categories, :name, :description, :created_at)", [
             "id_categories" => 1,
@@ -44,5 +48,105 @@ class DatabaseTest extends TestCase
         self::assertEquals("Gadget", $result[0]->name);
         self::assertEquals("Gadget Categories", $result[0]->description);
         self::assertEquals("2020-10-10 10:10:10", $result[0]->created_at);
+    }
+
+    public function testTransactionSuccess()
+    {
+        DB::transaction(function () {
+            DB::insert("insert into categories(id_categories, name, description, created_at) values(?, ?, ?, ?)", [
+                1,
+                "Gadget",
+                "Gadget Categories",
+                "2020-10-10 10:10:10"
+            ]);
+
+            DB::insert("insert into categories(id_categories, name, description, created_at) values(?, ?, ?, ?)", [
+                2,
+                "Food",
+                "Food Categories",
+                "2020-10-10 10:10:10"
+            ]);
+        });
+
+        $result = DB::select('select * from categories');
+        self::assertCount(2, $result);
+    }
+
+    public function testTransactionFailed()
+    {
+        try {
+            DB::transaction(function () {
+                DB::insert("insert into categories(id_categories, name, description, created_at) values(?, ?, ?, ?)", [
+                    1,
+                    "Gadget",
+                    "Gadget Categories",
+                    "2020-10-10 10:10:10"
+                ]);
+
+                DB::insert("insert into categories(id_categories, name, description, created_at) values(?, ?, ?, ?)", [
+                    1,
+                    "Food",
+                    "Food Categories",
+                    "2020-10-10 10:10:10"
+                ]);
+            });
+        } catch (QueryException $error) {
+            //expected 
+        }
+
+        $result = DB::select('select * from categories');
+        self::assertCount(0, $result);
+    }
+
+    public function testManualTransactionSuccess()
+    {
+        try {
+            DB::beginTransaction();
+            DB::insert("insert into categories(id_categories, name, description, created_at) values(?, ?, ?, ?)", [
+                    1,
+                    "Gadget",
+                    "Gadget Categories",
+                    "2020-10-10 10:10:10"
+                ]);
+
+                DB::insert("insert into categories(id_categories, name, description, created_at) values(?, ?, ?, ?)", [
+                    2,
+                    "Food",
+                    "Food Categories",
+                    "2020-10-10 10:10:10"
+                ]);
+                DB::commit();
+        } catch (QueryException $error) {
+            DB::rollBack();
+        }
+
+        $result = DB::select('select * from categories');
+        self::assertCount(2, $result);
+    }
+
+    public function testManualTransactionFailed()
+    {
+        try {
+            DB::beginTransaction();
+            DB::insert("insert into categories(id_categories, name, description, created_at) values(?, ?, ?, ?)", [
+                    1,
+                    "Gadget",
+                    "Gadget Categories",
+                    "2020-10-10 10:10:10"
+                ]);
+
+                DB::insert("insert into categories(id_categories, name, description, created_at) values(?, ?, ?, ?)", [
+                    1,
+                    "Food",
+                    "Food Categories",
+                    "2020-10-10 10:10:10"
+                ]);
+                DB::commit();
+        } catch (QueryException $error) {
+            DB::rollBack();
+        }
+
+        $result = DB::select('select * from categories');
+        self::assertCount(0, $result);
     }
 }
