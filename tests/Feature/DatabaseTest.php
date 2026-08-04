b552e12d6330d45830fbc06702402673cdd064a0
DwiPrema <madedwiprema08@gmail.com>
2026-08-04 14:48:18 +0800

    (laravel-database): Query Builder Update, UpdateOrInsert, and Increment Concepts

--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -314,5 +314,14 @@ class DatabaseTest extends TestCase
         });
     }
 
-    
+    public function testIncrement()
+    {
+        DB::table("counters")->where('id', '=', 'sample')->increment("counter", 1);
+
+        $collection = DB::table("counters")->where('id', '=', 'sample')->get();
+        self::assertCount(1, $collection);
+        $collection->each(function ($item) {
+            Log::info(json_encode($item));
+        });
+    }
 }
